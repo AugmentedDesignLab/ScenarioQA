db7e41968e898226a26c48c6ad38d7fd8ee31fe4
cat50105 <61899592+cat50105@users.noreply.github.com>
2024-04-01 23:09:26 -0700

    Added Additional Files

--- LiteLLM/main.py
+++ LiteLLM/main.py
@@ -4,13 +4,22 @@ from litellm import completion_with_retries
 import os
 
 ## set ENV variables
-os.environ["OPENAI_API_KEY"] = "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"
+os.environ["OPENAI_API_KEY"] = "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"
 os.environ["HUGGINGFACE_API_KEY"] = "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" 
+os.environ["COHERE_API_KEY"] = "stUXMZWW4Og2E6x5XLAOXvLMZjjQwJUcWGHNbJ7c" ## REPLACE THIS
+os.environ["GEMINI_API_KEY"] =  "AIzaSyDPSL6FngK-O45BgXn18rx5-4eVEJ4HAXw"
 
 messages = [{ "content": "Hello, how are you?","role": "user"}]
 
 # openai call
-response = completion(model="gpt-3.5-turbo", messages=messages)
+response = completion(model="gpt-4", messages=messages)
+
+# cohere call
+response = completion(model="command-r", messages=messages)
+response = completion(model="command-nightly", messages=messages)
+
+# Gemini call 
+response = completion(model="gemini/gemini-pro", messages=messages)
 
 # hugging face call
 # e.g. Call 'WizardLM/WizardCoder-Python-34B-V1.0' hosted on HF Inference endpoints
@@ -30,7 +39,7 @@ def hello():
 @app.route('/chat/completions', methods=["POST"])
 def api_completion():
     data = request.json
-    data["max_tokens"] = 256 # By default let's set max_tokens to 256
+    data["max_tokens"] = 1000 # By default let's set max_tokens to 256
     try:
         # COMPLETION CALL
         response = completion_with_retries(**data)
